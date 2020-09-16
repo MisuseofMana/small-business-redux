5341dd8f9aeada10b09494e1350a890029e86ed9
Sean Yager <contact@seanyager.com>
2020-09-16 18:15:14 -0500

    modifying actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,9 +1,14 @@
-const url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json"
-
 export const addCar = (car) => {
     return {
         type: 'ADD_CAR',
         value: car
+    }
+}
+
+export const setUser = (user) => {
+    return {
+        type: 'SET_USER',
+        value: user
     }
 }
 
@@ -18,19 +23,5 @@ export const deleteMake = (index) => {
     return {
         type: 'DELETE_MAKE',
         value: index
-    }
-}
-
-export const fetchMakes = () => {
-    return (dispatch) => {
-    fetch(url)
-    .then((res) => res.json())
-    .then((response) => {
-        const action = {
-            type: 'FETCH_MAKES',
-            value: response.Results
-        }
-        dispatch(action)
-    });
     }
 }
