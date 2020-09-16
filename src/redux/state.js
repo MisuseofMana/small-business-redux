0350c91e6900abc007a70cf646cfaff5bc6df7f1
Sean Yager <contact@seanyager.com>
2020-09-16 18:15:42 -0500

    created listings in the data

--- src/redux/state.js
+++ src/redux/state.js
@@ -146,5 +146,31 @@ export default {
         }
     ],
     makes:[],
+    listings: [
+       {
+         name: 'Home Slice Pizza',
+         description: 'A place to get a slice.',
+         hours:'11AM - 11PM',
+         address:"1415 S Congress Ave, Austin, TX, 78704"
+       },
+       {
+          name:'Elizabeth Street Cafe',
+          description:'A chill vibing place to get some bahn mi.',
+          hours:'11AM - 11PM',
+          address:'1501 S 1st St, Austin, TX 78704',
+       },
+       {
+          name:'Neighborhood Sushi',
+          description:'A secret hideout with super fresh sushi.',
+          hours:'11AM - 11PM',
+          address:'1716 S Congress Ave, Austin, TX 78704',
+       },
+       {
+          name:'Tommy Want Wingy',
+          description:'A streamline that sells dope wings and doper tots.',
+          hours:'11AM - 11PM',
+          address:'121 Pickle Rd, Austin, TX 78704',
+       }
+    ]
   };
   
