31d61f6d23ad161ca5c964feda05556018a1d515
Sean Yager <contact@seanyager.com>
2020-09-17 17:12:01 -0500

    removed old reducers and edited as needed

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,41 +1,16 @@
 import { combineReducers } from 'redux'
 
-const user = (state = [], action) => {
+const listings = (state = [], action) => {
     switch(action.type) {
-        case 'SET_USER':
-            let user = [ ...state ]
-            user.username = action.value
-            return user
-        default:
-            return state
-    }
-}
-
-const cars = (state = [], action) => {
-    switch(action.type) {
-        case 'ADD_CAR':
+        case 'ADD_LISTING':
             return [ ...state, action.value ]
-        case 'REMOVE_CAR':
-            let cars = [ ...state ]
-            cars.splice(action.value, 1)
-            return cars
+        case 'REMOVE_LISTING':
+            let listing = [ ...state ]
+            listing.splice(action.value, 1)
+            return listing
         default:
             return state
     }
 }
 
-const makes = (state = [], action) => {
-    switch(action.type) {
-        case 'FETCH_MAKES':
-            return action.value
-        case 'DELETE_MAKE':
-            let makes = [ ...state ]
-            console.log(action.value)
-            makes.splice(action.value, 1)
-            return makes
-        default:
-            return state
-    }
-}
-
-export default combineReducers({ user, cars, makes })
+export default combineReducers({ listings })
