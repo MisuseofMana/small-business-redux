0efe2dc794ae0dee70173393192bc5c657e2467f
Sean Yager <contact@seanyager.com>
2020-09-17 17:12:32 -0500

    removed old bindings and renamed elements

--- src/containers/Listings.js
+++ src/containers/Listings.js
@@ -1,19 +1,17 @@
 import { connect } from 'react-redux'
 import Listings from '../components/Listings'
-import { removeCar } from '../redux/actions'
+import { removeListing } from '../redux/actions'
 
 const mapStateToProps = (state) => {
     return {
         user: state.user,
-        cars: state.cars,
         listings: state.listings
     }
 }
 
-// add mapDispatchToProps function here
 const mapDispatchToProps = (dispatch) => {
     return {
-        removeCar: (index) => dispatch(removeCar(index))
+        removeListing: (index) => dispatch(removeListing(index))
     }
 }
 
